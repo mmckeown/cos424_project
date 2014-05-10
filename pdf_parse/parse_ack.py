4b5a0f86b3f839db8033b745c1d258a7970f30d9
Michael McKeown <mmckeown@princeton.edu>
2014-05-10 01:08:42 -0400

    Added many more tests for parsing acknowledgements. Fixed some bugs. Parser now done...moving on to cleaning up text and extracting features.

--- pdf_parse/parse_ack.py
+++ pdf_parse/parse_ack.py
@@ -3,6 +3,7 @@
 import sys, os
 import tempfile
 import re
+import string
 
 def cleanup_line (line) :
     # Remove page break characters
@@ -16,7 +17,7 @@ def cleanup_line (line) :
 
 def parse_ack (filename) :
     # Convert pdf file to text file
-    os.system("pdftotext -f 1 -l 20 " + filename)
+    os.system("pdftotext -f 1 -l 50 " + filename)
     
     # Get output filename
     txt_contents = filename[:-3] + "txt"
@@ -28,6 +29,7 @@ def parse_ack (filename) :
     page_line = 0
     page_count = 1
     acknowledgement_text = ""
+    success = True
     for line in contents:
         if not "\f" in line :
             page_line += 1
@@ -40,29 +42,45 @@ def parse_ack (filename) :
         # all characters to lower case to compare
         # headings to identify the acknowledgment section
         stripped_line = re.sub("\f", "", line)
-        stripped_line = re.sub(":", "", stripped_line)
+        stripped_line = re.sub("[0-9]", "", stripped_line)
+        stripped_line = re.sub("[%s]" % re.escape(string.punctuation), "", stripped_line)
+        stripped_line = re.sub("\xC2\xA0", "", stripped_line)
+        stripped_line = re.sub("\bM{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})\b", "", stripped_line)
+        stripped_line = re.sub("\bm{0,4}(cm|cd|d?c{0,3})(xc|xl|l?x{0,3})(ix|iv|v?i{0,3})\b", "", stripped_line)
         stripped_line = "".join(stripped_line.split())
         stripped_line = stripped_line.lower()
         if not in_ack :
             if stripped_line == "acknowledgment" or \
                stripped_line == "acknowledgement" or \
                stripped_line == "acknowledgments" or \
-               stripped_line == "acknowledgements" :
+               stripped_line == "acknowledgements" or \
+               stripped_line == "aknowledgment" or \
+               stripped_line == "aknowledgement" or \
+               stripped_line == "aknowledgments" or \
+               stripped_line == "aknowledgements" :
                 in_ack = True
                 ack_page_begin = page_count
                 acknowledgement_text += cleanup_line(line)
         else :
             if stripped_line == "tableofcontents" or \
+               stripped_line == "contents" or \
                stripped_line == "dedication" or \
                stripped_line == "abstract" or \
                stripped_line == "abstractofthedissertation" or \
                stripped_line == "abstractofdissertation" or \
                stripped_line == "abstractofthethesis" or \
                stripped_line == "abstractofthesis" or \
-               stripped_line == "chapter1" or \
+               stripped_line == "chapter" or \
                stripped_line == "chapteri" or \
+               stripped_line == "chapterone" or \
+               stripped_line == "chapterintroduction" or \
+               stripped_line == "chapteroneintroduction" or \
                stripped_line == "introduction" or \
-               stripped_line == "curriculumvitae" :
+               stripped_line == "curriculumvitae" or \
+               stripped_line == "vita" or \
+               stripped_line == "publications" or \
+               stripped_line == "presentations" or \
+               stripped_line == "publicationsandpresentations" :
                 # The beginning and end of an acknowledgment
                 # should not be on the same page, i.e. no two
                 # headings should be on the same page
@@ -73,6 +91,22 @@ def parse_ack (filename) :
                     # page, this is likely the table of contents
                     in_ack = False
                     acknowledgement_text = ""
+            # Restart if we found another acknowledgement
+            # heading, the first was likely in the ToC
+            elif stripped_line == "acknowledgment" or \
+               stripped_line == "acknowledgement" or \
+               stripped_line == "acknowledgments" or \
+               stripped_line == "acknowledgements" or \
+               stripped_line == "aknowledgment" or \
+               stripped_line == "aknowledgement" or \
+               stripped_line == "aknowledgments" or \
+               stripped_line == "aknowledgements" :
+                ack_page_begin = page_count
+                acknowledgement_text = cleanup_line(line) 
+            # If we exceed a certain page number, give up
+            elif (page_count - ack_page_begin) > 5 :
+                success = False
+                break;
             else :
                 acknowledgement_text += (cleanup_line(line))
     contents.close()
@@ -80,7 +114,7 @@ def parse_ack (filename) :
     # Remove the converted file
     os.remove(txt_contents)
 
-    return acknowledgement_text
+    return success, acknowledgement_text
 
 
 def parse_ack_main() :
@@ -92,7 +126,11 @@ def parse_ack_main() :
     
     # Parse the acknowledgement section
     print "Parsing acknowledgement section from " + pdf_path + "..."
-    print parse_ack(pdf_path)
+    success, ack_text = parse_ack(pdf_path)
+    if success:
+        print ack_text
+    else :
+        print "ERROR: Acknowledgement exceeded page limit, gave up"
 
 if __name__ == "__main__" :
     parse_ack_main ()
--- pdf_parse/run_regress.sh
+++ pdf_parse/run_regress.sh
@@ -5,7 +5,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Test set list
 TEST_SETS="sample
-           test1"
+           test1
+           test2
+           test3
+           test4
+           test5
+           test6
+           test7"
 
 # Run each test set
 ALL_PASS=1
